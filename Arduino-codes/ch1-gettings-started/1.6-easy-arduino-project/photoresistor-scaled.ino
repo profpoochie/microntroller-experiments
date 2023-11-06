1b5ae6bdabd734387a85cd9f8c2216f0db802d16
Marvin Borhillo <homepoochie@gmail.com>
2023-11-07 06:36:26 +0800

    Chapter 1 getting started

---
+++ Arduino-codes/ch1-gettings-started/1.6-easy-arduino-project/photoresistor-scaled.ino
@@ -0,0 +1,27 @@
+#include <arduino.h>
+
+const int sensorPin = A0;
+
+const int low = 200;
+const int high = 800;
+
+const int minDuration = 100;
+const int maxDuration = 1000;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  int delayval = analogRead(sensorPin);
+
+  delayval = map(delayval, low, high, minDuration, maxDuration);
+  delayval = constrain(delayval, minDuration, maxDuration);
+
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(delayval);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(delayval);
+}
---
+++ Arduino-codes/ch1-gettings-started/1.6-easy-arduino-project/photoresistor.ino
@@ -0,0 +1,17 @@
+#include <arduino.h>
+
+const int sensorPin = A0;
+
+void setup()
+{
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+
+void loop()
+{
+  int delayval = analogRead(sensorPin);
+  digitalWrite(LED_BUILTIN, HIGH);
+  delay(delayval);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(delayval);
+}

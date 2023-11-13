db20006d47f77254c6acdf54e00dacba3b22ea0c
Marvin Borhillo <homepoochie@gmail.com>
2023-11-13 20:43:20 +0800

    pushbutton pull-down and pull-up sketch

---
+++ Arduino-codes/ch5-digital-analog-input/5.1-using-a-switch/pushbutton-pull-up.ino
@@ -0,0 +1,22 @@
+/*
+  Pushbutton sketch a switch connected to pin 2 lights the LED on pin 13
+  Pull-up resistor version
+*/
+
+#include <Arduino.h>
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+
+void setup() {
+  pinMode(buttonPin, INPUT); // initialize the pushbutton pin as an input
+  pinMode(LED_BUILTIN, OUTPUT);       // initialize the LED pin as an output
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin); // read the state of the pushbutton value
+  if (buttonState == HIGH) {                // check if the pushbutton is pressed.
+    digitalWrite(LED_BUILTIN, LOW);        // turn LED on
+  } else {
+    digitalWrite(LED_BUILTIN, HIGH);       // turn LED off
+  }
+}
---
+++ Arduino-codes/ch5-digital-analog-input/5.1-using-a-switch/pushbutton.ino
@@ -0,0 +1,21 @@
+/*
+  Pushbutton sketch a switch connected to pin 2 lights the LED on pin 13
+*/
+
+#include <Arduino.h>
+
+const int buttonPin = 2;     // the number of the pushbutton pin
+
+void setup() {
+  pinMode(buttonPin, INPUT); // initialize the pushbutton pin as an input
+  pinMode(LED_BUILTIN, OUTPUT);       // initialize the LED pin as an output
+}
+
+void loop() {
+  int buttonState = digitalRead(buttonPin); // read the state of the pushbutton value
+  if (buttonState == HIGH) {                // check if the pushbutton is pressed.
+    digitalWrite(LED_BUILTIN, HIGH);        // turn LED on
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);         // turn LED off
+  }
+}
